1632455ba5c5e38f98ae14c78d929ae3a2ab93ee
asmahemmanuel <em1asmah@gmail.com>
2022-11-25 15:50:37 +0000

    changes

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>HSM PROGRAM OUTLINE</h1><hr /><br />
+            <h2>CONFERENCE: Testimony and Prophetic. 17th October, 12PM @ Yiri Lodge</h2>
+            <h2>DESTINY PRAYERS: Tuesdays @7PM Online Service</h2>
+            <h2>SCHOOL OF THE SPIRIT: Thursdays @7PM Online Service</h2>
+            <h2>FRIDAY IG LIVE: 1st&3rd Friday of Every Month @5pm</h2>
+            <h2>GOSPEL TRIVIA: Every Last Saturday of The Month @7pm</h2>
+        </div>
+    )
+}
+
+export default Header;
